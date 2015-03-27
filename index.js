c9b8910696c5608f835a4bfc720841f8f3473e93
Alejandro <alegonpov@gmail.com>
2015-03-27 15:19:01 -0600

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require(’express’)
+var fs
+= require(’fs’)
+var app = express();
+
+var quiz = JSON.parse(fs.readFileSync(’quiz.json’));
+app.set(’port’, (process.env.PORT || 8080)) /* used to be 5000 */
+app.use(express. s t a t i c (__dirname + ’/public’))
+
+app.get(’/’, function(request, response) {
+buffer = new Buffer(fs.readFileSync(’index.html’))
+response.send(buffer.toString(’utf8’))
+})
+
+app.listen(app.get(’port’), function() {
+console.log("Node app is running at localhost:" + app.get(’port’))
+})
+
